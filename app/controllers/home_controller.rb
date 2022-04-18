3d1e24c1d5364e2cf5aa0d0b7f642427f07c649d
Chris McKnight <cmckni3@gmail.com>
2022-04-18 16:42:03 -0500

    domain_name variable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
+    domain_name = 'google.com'
+    
     Resolv::DNS.open do |dns|
-      txt_records = dns.getresources 'cmcknight.io', Resolv::DNS::Resource::IN::TXT
+      txt_records = dns.getresources domain_name, Resolv::DNS::Resource::IN::TXT
       @dns_content = txt_records.map(&:strings).reject { |record| record.include?('keybase') }.join('')
     end
   end
